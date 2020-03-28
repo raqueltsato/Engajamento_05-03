6d00e22821fd0b0c041dfb40802d25d1e50e9ca8
Raquel <raquelsato1@gmail.com>
2020-03-28 10:09:26 -0300

    Correcao de referencia e retirada do metodo Contains

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -7,7 +7,7 @@ import java.util.*;
 public class App {
 
     private static Scanner sc = new Scanner(System.in);
-    private static HashSet<Aluno> listaAlunos = new HashSet<Aluno>();
+    private static Set<Aluno> listaAlunos = new HashSet<Aluno>();
 
     public static void main(String[] args) {
 
@@ -40,12 +40,7 @@ public class App {
         System.out.println("Insira o nome: ");
         String nome = sc.nextLine();
         Aluno a = new Aluno(nome);
-        if (listaAlunos.contains(a)) {
-            System.err.println("Aluno já cadastrado");
-        } else{
-            listaAlunos.add(new Aluno(nome));
-        }
-
+        listaAlunos.add(new Aluno(nome));
     }
 
 
